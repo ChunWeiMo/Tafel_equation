3a999011abfe1e10a100da84ea64394c15915623
chunwei.mo <chunwei.mo@gmail.com>
2025-07-09 13:29:53 -0700

    [UPDATE] clean up unnecessary comment

--- main.py
+++ main.py
@@ -24,16 +24,16 @@ class Voltage:
 
 class Resistance:
     @staticmethod
-    def poly(t, c_0, c_1, c_2, c_3):
-        return c_0 + c_1 * t + c_2 * t**2 + c_3 * t**3
+    def poly(t, c_0, c_1, c_2):
+        return c_0 + c_1 * t + c_2 * (t ** 2)
 
     @staticmethod
-    def exp(a, b, t: T):
-        return a * np.exp(b * t.t)
+    def exp(t, c_0, c_1, k):
+        return c_0 + c_1 * np.exp(k * t)
 
     @staticmethod
-    def ln(a, t: T):
-        return a * np.log(t.t+1)+1
+    def ln(t, c_0, c_1, c_2, k):
+        return c_0 + c_1 * np.log(k*t + c_2)
 
     @staticmethod
     def sigmoid(t, c_0, c_1, k, t0):
@@ -44,44 +44,29 @@ def main():
     t = T(100, 1)
 
     v1 = Voltage(0.1, 2.0, t)
-    v2 = Voltage(3.0, 5.0, t)
-
     resist_1 = np.zeros((11, len(t.t)))
-    resist_2 = np.zeros((11, len(t.t)))
-
     i_1 = np.zeros((11, len(t.t)))
-    i_2 = np.zeros((11, len(t.t)))
-
-    c_0 = 1.0
-    c_1_base = 0.02
-    c_2 = np.linspace(-1e-5, 1e-5, 5)
-    c_3 = 1e-6
 
     c_0 = 1
-    c_1_base = 0.5
-    k = 0.3
-    # k_values = np.linspace(0.1, 0.5, 5)
-    # t0 = 20.0
-    t0_values = np.linspace(20, 50, 5)
+    c_2_values = np.linspace(5e-7, 5e-5, 5)
+    t_final = t.t[-1]
 
     fig, ax1 = plt.subplots()
     ax2 = plt.twinx()
     v_normal = 1 + v1.potential / (v1.potential[-1]-v1.potential[0])
-    for n, t0 in enumerate(t0_values):
-        c_1 = c_1_base
-        # resist_1[n] = Resistance.poly(t.t, c_0, c_1, c, c_3)
-        resist_1[n] = Resistance.sigmoid(t.t, c_0, c_1, k, t0)
-        
+    for n, c_2 in enumerate(c_2_values):
+        c_1 = (0.6 - c_2 * (t_final ** 2))/t_final
+        resist_1[n] = Resistance.poly(t.t, c_0, c_1, c_2)
+
         i_1[n] = v1.potential/resist_1[n]
         i_normal = 1 + i_1[n] / (i_1[n][-1] - i_1[n][0])
 
-        ax1.plot(v_normal, i_normal, label=f"k = {k:0.2f}", linestyle="solid")
+        ax1.plot(v_normal, i_normal, label=f"c_2 = {c_2:0.2e}", linestyle="solid")
         ax2.plot(v_normal, resist_1[n], label=f"r = {resist_1[n][-1]:0.2f}", linestyle="dashed")
 
     ax1.set_xlabel("Normalized volt")
     ax1.set_ylabel("Normalized Current (—)")
-    # ax1.legend(loc='upper left')
-    # ax2.legend(loc='upper right')
+    ax1.legend(loc='upper left')
     ax2.set_ylabel("Normalized resistance (--)")
     plt.show()
 
