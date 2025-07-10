02d0cb39a6695c2b072a2c707eb9fffa78218412
chunwei.mo <chunwei.mo@gmail.com>
2025-07-10 16:02:42 -0700

    [UPDATE] add Current class and ohm method

--- main.py
+++ main.py
@@ -40,31 +40,52 @@ class Resistance:
         return c_0 + c_1 / (1 + np.exp(-k * (t - t0)))
 
 
+def tafel_equation(v, a, b):
+    """
+    Tafel equation: v = a + b * log10(i)
+    where:
+    - v is the overpotential
+    - a is the intercept
+    - b is the slope
+    - i is the current density
+    """
+    return a + b * np.log10(v)
+
+
+class Current:
+    @staticmethod
+    def ohm(v: Voltage, t: T):
+        resist_1 = np.zeros((11, len(t.t)))
+        i = np.zeros((11, len(t.t)))
+        i_normal = np.zeros((11, len(t.t)))
+
+        c_0 = 1
+        c_2_values = np.linspace(5e-7, 5e-5, 5)
+        t_final = t.t[-1]
+
+        for n, c_2 in enumerate(c_2_values):
+            c_1 = (0.6 - c_2 * (t_final ** 2))/t_final
+            resist_1[n] = Resistance.poly(t.t, c_0, c_1, c_2)
+
+            i[n] = v.potential/resist_1[n]
+            i_normal[n] = 1 + i[n] / (i[n][-1] - i[n][0])
+
+        return i_normal, resist_1, c_2_values
+
+
 def main():
     t = T(100, 1)
-
-    v1 = Voltage(0.1, 2.0, t)
-    resist_1 = np.zeros((11, len(t.t)))
-    i_1 = np.zeros((11, len(t.t)))
-
-    c_0 = 1
-    c_2_values = np.linspace(5e-7, 5e-5, 5)
-    t_final = t.t[-1]
+    v1 = Voltage(0.3, 1.0, t)
+    i_normal_all, resist_all, coefficients = Current.ohm(v1, t)
 
     fig, ax1 = plt.subplots()
     ax2 = plt.twinx()
-    v_normal = 1 + v1.potential / (v1.potential[-1]-v1.potential[0])
-    for n, c_2 in enumerate(c_2_values):
-        c_1 = (0.6 - c_2 * (t_final ** 2))/t_final
-        resist_1[n] = Resistance.poly(t.t, c_0, c_1, c_2)
 
-        i_1[n] = v1.potential/resist_1[n]
-        i_normal = 1 + i_1[n] / (i_1[n][-1] - i_1[n][0])
+    for i, r, k in zip(i_normal_all, resist_all, coefficients):
+        ax1.plot(v1.potential*1000, i, label=f"c_2 = {k:0.2e}", linestyle="solid")
+        ax2.plot(v1.potential*1000, r, label=f"r = {r[-1]:0.2f}", linestyle="dashed")
 
-        ax1.plot(v_normal, i_normal, label=f"c_2 = {c_2:0.2e}", linestyle="solid")
-        ax2.plot(v_normal, resist_1[n], label=f"r = {resist_1[n][-1]:0.2f}", linestyle="dashed")
-
-    ax1.set_xlabel("Normalized volt")
+    ax1.set_xlabel("overpatential (mV)")
     ax1.set_ylabel("Normalized Current (—)")
     ax1.legend(loc='upper left')
     ax2.set_ylabel("Normalized resistance (--)")
