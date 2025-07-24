2dcd68d20849028a69cb4803f78a7cc9dd1abd8f
chunwei.mo <chunwei.mo@gmail.com>
2025-07-24 12:43:51 -0700

    [UPDATE] add plot_current_resistance function

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ class Current:
     @staticmethod
     def ohm(v: Voltage, t: T):
         c_0 = 1
-        c_1 = 0.6
+        c_1 = 0.6  # final resistance is 1.6
         t_0 = 40
         k_values = np.linspace(0.01, 0.1, 5)
         t_final = t.t[-1]
@@ -84,7 +84,7 @@ class Current:
 
         Parameters
         ---
-        - v: overpotential
+        - v: overpotential  
         - i0: exchange current density
         - a: Tafel slope
 
@@ -94,12 +94,40 @@ class Current:
         return i0 * 10 ** (v/a)
 
 
+def plot_current_resistance(v1: Voltage, i_normal_all, i_mineral_normal_all, i_tafel_normal):
+    colors = ['blue', 'orange', 'green', 'red', 'purple']
+    fig1, ax1 = plt.subplots()
+    for i, color in zip(i_normal_all, colors):
+        ax1.plot(v1.potential*1000, i,  linestyle="solid", color=color)
+    ax1.set_title("Overpotential vs Normalized Current")
+    ax1.set_xlabel("overpatential (mV)")
+    ax1.set_ylabel("Current (—)")
+
+    fig2, ax2 = plt.subplots()
+    for i_mineral, color in zip(i_mineral_normal_all, colors):
+        ax2.plot(v1.potential*1000, i_mineral, linestyle="dashdot", color=color)
+    ax2.set_title("Overpotential vs Mineral Current")
+    ax2.set_xlabel("overpatential (mV)")
+    ax2.set_ylabel("Current (—)")
+
+    fig3, ax3 = plt.subplots()
+    for i_tafel, color in zip(i_tafel_normal, colors):
+        ax3.plot(v1.potential*1000, i_tafel,  linestyle="dotted", color=color)
+    ax3.set_title("Overpotential vs electrochemical current")
+    ax3.set_xlabel("overpatential (mV)")
+    ax3.set_ylabel("Current (—)")
+    plt.show()
+
+
 def main():
     t = T(100, 1)
     v1 = Voltage(0.3, 1.0, t)
     i_all, resist_all, coefficients = Current.ohm(v1, t)
+    i_all *= 0.0025  # adjust current to 10^-4mA/cm^2 order
+
     i_normal_all = np.zeros_like(i_all)
-    i_tafel = Current.tafel(v1.potential, 4e-2, 1)
+    i_tafel = Current.tafel(v1.potential, 1e-4, 1)
+    
     i_tafel_normal = np.zeros_like(i_all)
     i_mineral_all = np.zeros_like(i_all)
     i_mineral_normal_all = np.zeros_like(i_all)
@@ -109,9 +137,10 @@ def main():
         i_mineral_normal_all[n] = (i_mineral_all[n] - i_mineral_all[n][0]) / (i[-1] - i[0])
         i_tafel_normal[n] = i_normal_all[n] - i_mineral_normal_all[n]
 
+    colors = ['blue', 'orange', 'green', 'red', 'purple']
+
     fig1, ax1 = plt.subplots()
     ax3 = ax1.twinx()
-    colors = ['blue', 'orange', 'green', 'red', 'purple']
     for i, r, k, color in zip(i_normal_all, resist_all, coefficients, colors):
         ax1.plot(v1.potential*1000, i, label=f"k = {k:0.2e}", linestyle="solid", color=color)
         ax3.plot(v1.potential*1000, r, label=f"r = {r[-1]:0.2f}", linestyle="dashed")
@@ -121,17 +150,7 @@ def main():
     ax1.legend(loc='upper left')
     ax3.set_ylabel("Normalized resistance (--)")
 
-    fig2, ax2 = plt.subplots()
-    for i, i_mineral, i_tafel, k, color in zip(i_normal_all, i_mineral_normal_all, i_tafel_normal, coefficients, colors):
-        ax2.plot(v1.potential*1000, i, label=f"k = {k:0.2e}", linestyle="solid", color=color)
-        ax2.plot(v1.potential*1000, i_mineral, label=f"delta_i, k = {k:0.2e}", linestyle="dashdot", color=color)
-        ax2.plot(v1.potential*1000, i_tafel, label="tafal current", linestyle="dotted", color=color)
-    ax2.set_title("Mineral and Tafel Current vs Overpotential")
-    ax2.set_xlabel("overpatential (mV)")
-    ax2.set_ylabel("Normalized Current (—)")
-    ax2.legend(loc='upper left')
-
-    plt.show()
+    plot_current_resistance(v1, i_normal_all, i_mineral_normal_all, i_tafel_normal)
 
 
 if __name__ == "__main__":
